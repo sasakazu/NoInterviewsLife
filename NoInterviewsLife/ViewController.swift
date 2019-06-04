1189a43b399e532f0b351817c36d8403bd4af6ff
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-06-04 16:45:35 +0900

    first commit

---
+++ NoInterviewsLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoInterviewsLife
+//
+//  Created by 笹倉一也 on 2019/06/04.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
